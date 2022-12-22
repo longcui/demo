d9f32fa3a7e1bdd36ea138740fe4386db9c31c21
Long Cui <long.cui@kongsbergdigital.com>
2022-12-22 11:50:16 +0800

    still not working, it still goes to keycloak.json

--- src/main/java/com/example/demo/GlobalSecurityConfiguration.java
+++ src/main/java/com/example/demo/GlobalSecurityConfiguration.java
@@ -1,10 +1,15 @@
 // package com.example.demo;
 
+// import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 // import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+// import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 // import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+// import org.keycloak.adapters.KeycloakConfigResolver;
 // import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.context.annotation.Bean;
 // import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 // import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 // import org.springframework.security.core.session.SessionRegistryImpl;
 // import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 // import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
@@ -17,9 +22,13 @@
 //      */
 //     @Autowired
 //     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//         auth.authenticationProvider(keycloakAuthenticationProvider());
+//         // auth.authenticationProvider(keycloakAuthenticationProvider());
+//         KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+//         keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(new SimpleAuthorityMapper());
+//         auth.authenticationProvider(keycloakAuthenticationProvider);
 //     }
 
+//     @Bean
 //     @Override
 //     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
 //         // // TODO Auto-generated method stub
@@ -36,13 +45,19 @@
 //     @Override
 //     protected void configure(HttpSecurity http) throws Exception
 //     {
-//        super.configure(http);
-//        http
-//                .authorizeRequests()
-//             //    .antMatchers("/**").hasRole("default-roles-dev-idc")
-//             //  .anyRequest().permitAll();
-//                .antMatchers("/**").authenticated()
-//              .anyRequest().permitAll();
+//         super.configure(http);
+//         http
+//                 .authorizeRequests()
+//                 //    .antMatchers("/**").hasRole("default-roles-dev-idc")
+//                 //  .anyRequest().permitAll();
+//                 .antMatchers("/**").authenticated()
+//                 .anyRequest().permitAll();
+//     }
+    
+//     // @Bean ?
+//     // @Autowired ?
+//     public KeycloakConfigResolver setKeycloakConfigResolver() {
+//         return new KeycloakSpringBootConfigResolver();
 //     }
 
 
